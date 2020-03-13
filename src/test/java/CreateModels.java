397afb5e17ff56b42251f158035e1b9fab847885
bLozanoUPM <borja.lozano.alvarez@alumnos.upm.es>
2020-03-13 11:09:47 +0100

    test to project documents added

--- src/test/java/CreateModels.java
+++ src/test/java/CreateModels.java
@@ -15,16 +15,29 @@ public class CreateModels {
 
     @Test
     public void createModels() throws IOException {
-
-
-//        LibrairyClient.createModel("test","EN","1.0","/librairy/resources/en/sml9/train/s1.csv",100);
-
-        for (int i = 1; i < 4; i++) {
-            int version=1;
-            for(Integer n: new int[]{50,100,300,500}){
-                LibrairyClient.createModel("en_sml3_s"+i,"EN",(version++) +".0","/librairy/resources/en/sml3/train/s"+i+".csv",n);
-            }
-        }
+        for(String lang: new String[]{"en","es"})
+            Files.list(Paths.get(resources+"/"+lang+"/"))
+                    .filter(Files::isDirectory)
+                    .forEach(d->{
+                        try {
+                            Files.list(Paths.get(d.toString()+"/train/"))
+                            .forEach(t->{
+                                LOG.info(t.toString());
+                                String model = d.getFileName().toString();
+                                int version = 1;
+                                for(Integer n: new int[]{50,100,300,500}){
+                                    String model_name = t.getFileName().toString();
+                                    LibrairyClient.createModel(lang+"_"+model+"_"+model_name.substring(0, model_name.lastIndexOf('.')),
+                                            lang.toUpperCase(),
+                                            (version++) +".0",
+                                            "/librairy/resources/en/"+model+"/train/" + model_name,
+                                            n);
+                                }
+                            });
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
 
     }
 }
---
+++ src/test/java/ProjectDocuments.java
@@ -0,0 +1,2 @@
+public class ProjectDocuments {
+}
