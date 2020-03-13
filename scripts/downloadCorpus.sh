e70c0b5d60244834ce1a58cf864e49288134b05a
bLozanoUPM <borja.lozano.alvarez@alumnos.upm.es.com>
2020-03-13 11:08:04 +0100

    download resources now work

--- scripts/downloadCorpus.sh
+++ scripts/downloadCorpus.sh
@@ -1,4 +1,8 @@
-curl "https://drive.google.com/uc?export=download&id=1g6LotttRN89FicpwlWdJCVjAV5JzTnXm" --output ./src/main/resources
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id=1g6LotttRN89FicpwlWdJCVjAV5JzTnXm' -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
 
-unzip ./src/main/resources/corpora.zip
-rm ./src/main/resources/corpora.zip
+wget --load-cookies cookies.txt -O  corpora.zip\
+     'https://docs.google.com/uc?export=download&id=1g6LotttRN89FicpwlWdJCVjAV5JzTnXm&confirm='$(<confirm.txt)
+
+unzip corpora.zip
+rm corpora.zip
--- scripts/downloadSplits.sh
+++ scripts/downloadSplits.sh
@@ -1,4 +1,9 @@
-curl "https://drive.google.com/uc?export=download&id=192ieQmiNR1v67_0UUYZInDJcs1GvHnL_" --output ./src/main/resources
+wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id=192ieQmiNR1v67_0UUYZInDJcs1GvHnL_' -O- \
+     | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
 
-unzip ./src/main/resources/acquis.zip
-rm ./src/main/resources/acquis.zip
+wget --load-cookies cookies.txt -O  acquis.zip\
+     'https://docs.google.com/uc?export=download&id=192ieQmiNR1v67_0UUYZInDJcs1GvHnL_&confirm='$(<confirm.txt)
+
+
+unzip acquis.zip
+rm acquis.zip
